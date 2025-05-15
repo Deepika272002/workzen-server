f2c2c02e5b2f1b4605b467bccb72f4825654900d
Sagar-138 <kumarsagar3870@gmail.com>
2025-05-15 23:04:02 +0530

    first commit

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const io = initializeSocket(httpServer);
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+   origin: process.env.FRONTEND_URL?.replace(/\/$/, "")|| "http://localhost:5173",
   credentials: true
 }));
 app.use(express.json());
